82ca5484b84f3e395c3457e3c452ef62a4495ef0
Jakob Blaschke <jakobblaschke@yahoo.de>
2020-07-11 00:54:42 +0200

    Add routes for login/signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
 
+  get '/signup', to: 'users#new'
   resources :users, only: [:show, :new, :create]
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get 'logout', to: 'sessions#destroy'
+
   root 'users#new'
 end

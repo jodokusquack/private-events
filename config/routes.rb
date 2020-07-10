4825d5571073d842c6744e5927d3d07a0331992c
Jakob Blaschke <jakobblaschke@yahoo.de>
2020-07-11 01:10:14 +0200

    Add Sessions model and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   get '/signup', to: 'users#new'
   resources :users, only: [:show, :new, :create]
 

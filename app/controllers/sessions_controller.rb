c41f508de9a38ffe0b2acf3e88c80b74458af6b9
Jakob Blaschke <jakobblaschke@yahoo.de>
2020-07-11 01:22:49 +0200

    Make Sessions Controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,22 @@ class SessionsController < ApplicationController
   end
 
   def create
+    # find the user from email
+    @current_user = User.find_by(email: params[:session][:email])
+
+    if @current_user
+      session[:user_id] = @current_user.id
+      redirect_to @current_user
+    else
+      flash[:alert] = "No User with that Email exists!"
+      render 'new'
+    end
   end
 
   def destroy
+    session.delete(:user_id)
+    @current_user = nil
+
+    redirect_to :root
   end
 end

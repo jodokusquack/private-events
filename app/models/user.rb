c547b849a1f152daadd937756963898cb6e76937
Jakob Blaschke <jakobblaschke@yahoo.de>
2020-07-11 19:24:39 +0200

    Add association between hosts(users) and events

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   validates :name, :email, :address, presence: true
   validates :email, uniqueness: true
+
+  has_many :events, foreign_key: "host_id"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_11_170233) do
+ActiveRecord::Schema.define(version: 2020_07_11_170908) do
 
   create_table "events", force: :cascade do |t|
     t.datetime "date"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 2020_07_11_170233) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "description"
+    t.integer "host_id"
+    t.index ["host_id"], name: "index_events_on_host_id"
   end
 
   create_table "users", force: :cascade do |t|

b2c0589f108e4ed6417ca371a9d62fca3aa69dfb
Jakob Blaschke <jakobblaschke@yahoo.de>
2020-07-25 01:01:24 +0200

    Split events according to date

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,13 @@ class User < ApplicationRecord
   # we need a source here to say the key to look for in the "invitations"
   # table is not "event_id" but "attended_event_id"
   has_many :attended_events, through: :invitations, class_name: "Event", source: :event
+
+
+  def previous_events
+    attended_events.where("date < :current_date", current_date: Date.today).order(date: :desc)
+  end
+
+  def upcoming_events
+    attended_events.where("date >= :current_date", current_date: Date.today).order(date: :asc)
+  end
 end
